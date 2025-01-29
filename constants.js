d281f416ac745d42aa250e5b687903c2188ee0bb
ＨＢＳ <95830789+heroblineg@users.noreply.github.com>
2025-01-29 20:58:21 +0900

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,37 +1,37 @@
 var ICONS = [
   {
     idx: 0,
+    title: "AM",
+    path: "/resources/icons/am.svg",
+  },
+  {
+    idx: 1,
     title: "Apple",
     path: "/resources/icons/apple.svg",
   },
   {
-    idx: 1,
+    idx: 2,
     title: "Ubuntu",
     path: "/resources/icons/ubuntu.svg",
   },
   {
-    idx: 2,
+    idx: 3,
     title: "Fedora",
     path: "/resources/icons/fedora.svg",
   },
   {
-    idx: 3,
+    idx: 4,
     title: "Linux",
     path: "/resources/icons/linux.svg",
   },
   {
-    idx: 4,
+    idx: 5,
     title: "Debian",
     path: "/resources/icons/debian.svg",
   },
   {
-    idx: 5,
+    idx: 6,
     title: "Arch",
     path: "/resources/icons/arch.svg",
-  },
-  {
-    idx: 6,
-    title: "Kali",
-    path: "/resources/icons/kali.svg",
   },
 ];
